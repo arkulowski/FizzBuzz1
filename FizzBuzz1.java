67c16a24b2603279e1d32ef62b3dfa4c83a638b1
arkulowski <arkulowski1@quinnipiac.edu>
2023-03-23 15:59:42 -0400

    reset

--- FizzBuzz1.java
+++ FizzBuzz1.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 
 public class FizzBuzz1 {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); 
+        boolean fizz =  n % 3 == 0;
+        boolean buzz = n % 5 == 0; 
+        if()
+        {
+            System.out.println("Fizz");
+        }
+        if()
+        {
+            System.out.println("Buzz");
+        }
+        if(!Fizz && !Buzz)
+        {
+            System.out.println(n)
+        }
+        
 
     }
 }
